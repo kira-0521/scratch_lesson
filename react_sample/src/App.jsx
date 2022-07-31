f82139895fe58c380faece8fa6f11741c70a0646
kira-0521 <kiratanaka0521@gmail.com>
2022-07-31 17:17:59 +0900

    [ add ] reactでの実装

--- react_sample/src/App.jsx
+++ react_sample/src/App.jsx
@@ -1,32 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])
+
+  const BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+  const fetchUsers = async () => {
+    const { data } = await axios.get(`${BASE_URL}/users`)
+    return data
+  }
+
+  useEffect(() => {
+    fetchUsers().then((users) => {
+      setUsers(users)
+    })
+  }, [])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className='App'>
+      <h1 class='title'>scratch 開発</h1>
+      <div class='wrapper'>
+        <h2 class='wrapper_title'>ユーザーリスト</h2>
+        <button
+          onClick={() => {
+            console.log('click')
+          }}>
+          ボタン
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <ul class='wrapper_user-list'>
+          {users.map((user) => (
+            <li>{user.name}</li>
+          ))}
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
