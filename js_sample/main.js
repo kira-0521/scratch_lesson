9d2006e9def7090099ee921fea4a260afa81befc
kira-0521 <kiratanaka0521@gmail.com>
2022-07-31 14:54:18 +0900

    [ refactor ] main.JavaScriptのみに集約

--- js_sample/main.js
+++
@@ -1,6 +0,0 @@
-import { fetchUsers } from './scripts/api.js'
-import { insertUserData } from './scripts/user.js'
-;(async () => {
-  const users = await fetchUsers()
-  await insertUserData(users)
-})()
